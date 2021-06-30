ef9c4ddad433f4dfdcbff2aab3b118f9de3deeda
web2te <55467080+schen2141639@users.noreply.github.com>
2021-06-29 23:48:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ var express = require("express");
 var app = express();
 
 
- 
-app.set("views", path.join(__dirname, "views"));
-
-
 app.get("/", (req, res) => {
     res.send("12345678921324");
 });
