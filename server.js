8a988c9e880f33211b516855a918c4c98bc3c12e
web2te <55467080+schen2141639@users.noreply.github.com>
2021-06-30 14:41:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
+server.js
+
 const express = require("express");
 
 const path = require("path");
 
 const bodyParser = require("body-parser");
 
-// const getModel = require("./model");
-
-const exphbs = require('express-handlebars');
+const getModel = require("./model");
 
 const crypto = require("crypto");
 
@@ -14,24 +14,16 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-// configure the handlebars view engine 
-// .hbs is set as the extension
-// base layout template is set to main  (main.hbs is in views/layouts)
-// base template stores all the header and footer information
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set("views", path.join(__dirname, "views"));
 
-// set app's view engine as handlebars
-app.set('view engine', 'hbs');
 
-// generally filename main.hbs is used for base template. So the code in main is transfered to 
-// home.hbs which extends main.hbs and used to greet the user
-app.get('/', (req, res) => {
-        res.render('home', { username: 'Joe' })
-})
 
-// login.hbs template extends the base template main to show a login form
-app.get('/login', (req, res) => {
-    res.render('login');
+app.get("/", (req, res) => {
+
+res.send("12345678921324");
+
 });
 
-app.listen(5000);
+
+
+app.listen(HTTP_PORT);
