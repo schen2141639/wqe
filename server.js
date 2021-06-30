c3c31951465aa0d1a0b9bea1ecfb6dfbec12e626
web2te <55467080+schen2141639@users.noreply.github.com>
2021-06-30 14:55:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,11 @@
-server.js
-
-const express = require("express");
-
-const path = require("path");
-
-const bodyParser = require("body-parser");
-
-const getModel = require("./model");
-
-const crypto = require("crypto");
-
-const cookieParser = require("cookie-parser");
-
-const app = express();
-
-app.set("views", path.join(__dirname, "views"));
-
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
 
 
 app.get("/", (req, res) => {
-
-res.send("12345678921324");
-
+    res.send("12345678921324");
 });
-
 
 
 app.listen(HTTP_PORT);
